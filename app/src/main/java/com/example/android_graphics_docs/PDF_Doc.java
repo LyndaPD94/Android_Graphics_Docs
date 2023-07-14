4133d3312529fdb552694cb3e99fef54a802bb4a
Lynda Douglas <lynda.p.douglas@gmail.com>
2023-07-14 17:28:22 -0600

    Initial commit 14-07-23

---
+++ app/src/main/java/com/example/android_graphics_docs/PDF_Doc.java
@@ -0,0 +1,9 @@
+package com.example.android_graphics_docs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PDF_Doc extends AppCompatActivity {
+    //PdfDocument pdfDocument=new PdfDocument();
+    //PdfRenderer pdfRenderer=new PdfRenderer(ParcelFileDescriptor.adoptFd(1));
+    //pdfRenderer.openPage(1);
+}
